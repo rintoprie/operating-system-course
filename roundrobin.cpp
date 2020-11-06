cfeb5f449631ddf0a9a9450572bbc47727b9d3e1
rintoprie <rintoprie@users.noreply.github.com>
2020-11-06 20:00:10 +0700

    Create roundrobin.cpp

---
+++ roundrobin.cpp
@@ -0,0 +1,148 @@
+/*
+Example of Round Robin Scheduling in C++
+https://www.geeksforgeeks.org/program-round-robin-scheduling-set-1/
+
+Output:
+Time quantum = 1
+Processes 	Burst Time 	Waiting Time 	Turn Around Time
+1        	3     		4         		7
+2        	4     		6         		10
+3        	3     		6         		9
+Average waiting time = 5.33333
+Average turn around time = 8.66667
+
+Explanation:
+Processes: Process ID
+Burst Time: Time needed to finish the process
+Waiting Time: Time Difference between turn around time and burst time (Waiting Time = Turn Around Time – Burst Time)
+Turn Around Time: Time Difference between completion time and arrival time (Turn Around Time = Completion Time – Arrival Time)
+
+Timeline from simulation:
+Process Execution:	P1 	P2 	P3 	P1 	P2 	P3 	P1 	P2 	P3 	P2	(Completed)
+Time unit: 			0---1---2---3---4---5---6---7---8---9---10
+*/
+#include <iostream> 
+using namespace std; 
+
+// Function to find the waiting time for all 
+// processes 
+void findWaitingTime(int processes[], int n, 
+			int bt[], int wt[], int quantum) 
+{ 
+	// Make a copy of burst times bt[] to store remaining 
+	// burst times. 
+	int rem_bt[n]; 
+	for (int i = 0 ; i < n ; i++) 
+		rem_bt[i] = bt[i]; 
+
+	int t = 0; // Current time 
+
+	// Keep traversing processes in round robin manner 
+	// until all of them are not done. 
+	while (1) 
+	{ 
+		bool done = true; 
+
+		// Traverse all processes one by one repeatedly 
+		for (int i = 0 ; i < n; i++) 
+		{ 
+			// If burst time of a process is greater than 0 
+			// then only need to process further 
+			if (rem_bt[i] > 0) 
+			{ 
+				done = false; // There is a pending process 
+
+				if (rem_bt[i] > quantum) 
+				{ 
+					// Increase the value of t i.e. shows 
+					// how much time a process has been processed 
+					t += quantum; 
+
+					// Decrease the burst_time of current process 
+					// by quantum 
+					rem_bt[i] -= quantum; 
+				} 
+
+				// If burst time is smaller than or equal to 
+				// quantum. Last cycle for this process 
+				else
+				{ 
+					// Increase the value of t i.e. shows 
+					// how much time a process has been processed 
+					t = t + rem_bt[i]; 
+
+					// Waiting time is current time minus time 
+					// used by this process 
+					wt[i] = t - bt[i]; 
+
+					// As the process gets fully executed 
+					// make its remaining burst time = 0 
+					rem_bt[i] = 0; 
+				} 
+			} 
+		} 
+
+		// If all processes are done 
+		if (done == true) 
+		break; 
+	} 
+} 
+
+// Function to calculate turn around time 
+void findTurnAroundTime(int processes[], int n, 
+						int bt[], int wt[], int tat[]) 
+{ 
+	// calculating turnaround time by adding 
+	// bt[i] + wt[i] 
+	for (int i = 0; i < n ; i++) 
+		tat[i] = bt[i] + wt[i]; 
+} 
+
+// Function to calculate average time 
+void findavgTime(int processes[], int n, int bt[], 
+									int quantum) 
+{ 
+	int wt[n], tat[n], total_wt = 0, total_tat = 0; 
+
+	// Function to find waiting time of all processes 
+	findWaitingTime(processes, n, bt, wt, quantum); 
+
+	// Function to find turn around time for all processes 
+	findTurnAroundTime(processes, n, bt, wt, tat); 
+
+	// Display processes along with all details 
+	cout << "Processes "<< " Burst time "
+		<< " Waiting time " << " Turn around time\n"; 
+
+	// Calculate total waiting time and total turn 
+	// around time 
+	for (int i=0; i<n; i++) 
+	{ 
+		total_wt = total_wt + wt[i]; 
+		total_tat = total_tat + tat[i]; 
+		cout << " " << i+1 << "\t\t" << bt[i] <<"\t "
+			<< wt[i] <<"\t\t " << tat[i] <<endl; 
+	} 
+
+	cout << "Average waiting time = "
+		<< (float)total_wt / (float)n; 
+	cout << "\nAverage turn around time = "
+		<< (float)total_tat / (float)n; 
+} 
+
+// Driver code 
+int main() 
+{ 
+	// process id's 
+	int processes[] = { 1, 2, 3 }; 
+	int n = sizeof processes / sizeof processes[0]; 
+
+	// Burst time of all processes 
+	int burst_time[] = {3, 4, 3}; 
+
+	// Time quantum 
+	int quantum = 1; 
+	cout << "Time quantum = " << quantum << "\n";
+	findavgTime(processes, n, burst_time, quantum); 
+	return 0; 
+} 
